8718dd8944babbf92bf2a5f5736bd9207d695cc5
harshil62-cell <harshil2461@gmail.com>
2025-05-30 23:46:55 +0530

    lecture-13 basic api creation

---
+++ Project_01/index.js
@@ -0,0 +1,30 @@
+//lecture-13
+const express=require('express');
+const users=require('./MOCK_DATA.json');
+const app=express();
+const port=8000;
+
+//Routes
+//List all users 
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+
+//api sending html (server side rendering)
+app.get('/users',(req,res)=>{
+    const html=`
+    <ul>
+        ${users.map((user)=>{return `<li>${user.first_name}</li>` })}
+    </ul>
+    `
+    res.send(html);
+})
+
+//get specific user with id 
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);//as this will be a string but we want number so convert it to number
+    const user=users.find((user)=>user.id===id);
+    return res.json(user);
+})
+
+app.listen(port,()=>console.log(`server started on port: ${port}`));
