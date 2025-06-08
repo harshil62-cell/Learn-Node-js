075b908c04ea2b5f8181a43f4f628204c8a6a887
harshil62-cell <harshil2461@gmail.com>
2025-06-08 23:40:58 +0530

    mongoose

--- Project_01/index.js
+++ Project_01/index.js
@@ -2,8 +2,46 @@
 const express=require('express');
 let users=require('./MOCK_DATA.json');
 const fs=require('fs');
+const mongoose=require('mongoose');
 const app=express();
 const port=8000;
+
+//connection with db this returns a promise
+mongoose.connect('mongodb://127.0.0.1:27017/database_name')
+        .then(()=>{
+        console.info("Database connection successfull");
+        })
+        .catch((err)=>{
+            console.error("Error",err);
+        })
+
+//schema
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type:String,
+        required:false,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    jobTitle:{
+        type:String
+    },
+    gender:{
+        type:String
+    },
+    
+},{timestamps:true})
+
+//model
+const User=mongoose.model('user',userSchema);
+//now using this User class we can interact with our database
 
 //middleware-plugin
 app.use(express.urlencoded({extended:false}));
@@ -43,10 +81,11 @@ app.use((req,res,next)=>{
 
 //Routes
 //List all users 
-app.get('/api/users',(req,res)=>{
+app.get('/api/users',async (req,res)=>{
     res.setHeader("X-myName","Harshil Vasani");//custom response header
     //Always add X to custom headers to identify that they are custom headers
-    return res.json(users);
+    const allDbUsers=await User.find({});
+    return res.json(allDbUsers);
 })
 
 //api sending html (server side rendering)
@@ -60,9 +99,10 @@ app.get('/users',(req,res)=>{
 })
 
 //get specific user with id 
-app.get('/api/users/:id',(req,res)=>{
-    const id=Number(req.params.id);//as this will be a string but we want number so convert it to number
-    const user=users.find((user)=>user.id===id);
+app.get('/api/users/:id',async(req,res)=>{
+    // const id=Number(req.params.id);//as this will be a string but we want number so convert it to number
+    // const user=users.find((user)=>user.id===id);
+    const user=await User.findById(req.params.id);
     if(!user){
         return res.status(404).json({message:`user with id: ${id} not found`});
     }
@@ -70,16 +110,26 @@ app.get('/api/users/:id',(req,res)=>{
 })
 
 //post user
-app.post('/api/user',(req,res)=>{
+app.post('/api/user',async(req,res)=>{
     const body=req.body;
     if(!body || !body.first_name || !body.last_name || !body.job_title || !body.email || !body.gender){
         return res.status(400).json({message:"All fields are required"});
     }
     console.log("body ",body);
-    users.push({...body,id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        return res.status(201).json({status:"done"});
+    const result=await User.create({
+        firstName:body.first_name,
+        lastName:body.last_name,
+        email:body.email,
+        jobTitle:body.job_title,
+        gender:body.gender,
     })
+    console.log(result);
+    return res.status(201).json({message:"success"});
+    //below we were working with filesystem now we are working with db
+    // users.push({...body,id:users.length+1});
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    //     return res.status(201).json({status:"done"});
+    // })
 })
 //homework complete below API
 app.
